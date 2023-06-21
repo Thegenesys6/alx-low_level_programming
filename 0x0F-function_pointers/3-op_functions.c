c44c43ddeebd6e093a88eda2dfdb75d3ea88ed1e
Thegenesys6 <136111640+Thegenesys6@users.noreply.github.com>
2023-06-21 14:13:27 -0700

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - this calculates sum of 2 integers
+ * @a- integer 1
+ * @b- integer 2
+ *
+ * Return: sum a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - this calculates difference of 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: difference a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - this calculates product of 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - this calculates division of 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - this calculates the remainder of division of two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: remainder of a divided b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
